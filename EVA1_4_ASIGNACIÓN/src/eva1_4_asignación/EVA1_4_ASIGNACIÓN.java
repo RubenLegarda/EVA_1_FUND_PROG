e751e7779c1cde6982040cc84a18bb609e4e370e
RubenLegarda <159494601+RubenLegarda@users.noreply.github.com>
2024-02-29 16:27:57 -0600

    Add files via upload

---
+++ EVA1_4_ASIGNACIÓN/src/eva1_4_asignación/EVA1_4_ASIGNACIÓN.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva1_4_asignación;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA1_4_ASIGNACIÓN {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int edad; //DECLARACIÓN
+        String nombre;
+        String carrera;
+        nombre = "Ruben Legarda";
+        carrera = "Sistemas Comptacionales";
+        edad = 18; //ASIGNACION NO "IGUALDAD" -- EL SIMBOLO "=" SIGNIFICA ASIGNAR ALGO
+        System.out.println("Tu edad es:");
+        System.out.println(edad);
+        System.out.println("Tu Nombre es:");
+        System.out.println(nombre);
+        System.out.println("Tu Carrera es:");
+        System.out.println(carrera);
+    }
+    
+}
