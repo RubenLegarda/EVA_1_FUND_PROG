72cad01d89f16156e54d3a220007c3293985dc5d
RubenLegarda <159494601+RubenLegarda@users.noreply.github.com>
2024-02-29 00:53:16 -0600

    Add files via upload

---
+++ EVA1_2_IDENTIFICADORES/src/main/java/com/mycompany/eva1_2_identificadores/EVA1_2_IDENTIFICADORES.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_2_identificadores;
+
+/**
+ *
+ * @author Cuenta Personal
+ */
+public class EVA1_2_IDENTIFICADORES {
+
+    public static void main(String[] args) {
+        //VARIABLES;
+        //TIPO DE DATO + NOMBRE DEL IDENTIFICADOR
+        //short int byte long --> enteros
+        //float double --> numeros con decimal
+        //char --> character --> caracteres
+        //boolean --> verdadero o falso
+        //String --> cadenas de texto
+        int edad; //declaración de una variable
+        String nombre;
+        double salario;
+        
+        //ASIGNACIÓN DE VALORES
+        //IDENTIFICADOR = VALOR
+        //se coloca el valor 50 dentro de la variable edad
+        edad = 50;
+        nombre = "Ruben Legarda Reyna";
+        salario = 123.3;
+        //COMO LOS USAMOS:
+        System.out.println(nombre);
+        System.out.println(edad);
+        System.out.println(salario);
+    }
+}
